1784eb422457b4ea535c530c84d5f0655bcb39d6
moz-sec <m0253c@gmail.com>
2024-04-22 15:20:55 +0900

    feat: parse command arguments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,58 @@
+use clap::{Parser, ValueEnum};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Compress or Decompress file name
+    #[arg(value_name = "FILE")]
+    file: String,
+
+    /// Multiple File Designation
+    #[arg(last=true)]
+    multiple_files: Vec<String>,
+
+    /// Whether to decompress the input
+    #[arg(short, long)]
+    decompress: bool,
+
+    /// Put passwords on compressed files
+    #[arg(short, long)]
+    encrypt: bool,
+
+    #[arg(short, long)]
+    #[clap(value_enum, default_value_t=Format::Zip)]
+    format: Format,
+
+    /// Whether to recursively greet
+    #[arg(short, long)]
+    recursive: bool,
+
+    /// Verbose mode
+    #[arg(short, long)]
+    verbose: bool,
+
+    /// Number of compressions to perform
+    #[arg(short, long, default_value = "6")]
+    count: u32,
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+enum Format {
+    Gzip,
+    Zip,
+}
+
 fn hello() -> String {
     "Hello, world!".to_string()
 }
 
 fn main() {
+    let args = Args::parse();
+
+    println!("Hello, world!");
+
+
     println!("{}", hello());
 }
 
